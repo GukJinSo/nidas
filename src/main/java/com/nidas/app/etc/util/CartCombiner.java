cedfa6bf3895c6071383c51c9a2ef14c8393993a
Gu <Gu@211.229.157.149>
2022-01-01 18:28:46 +0900

    01.01

--- src/main/java/com/nidas/app/etc/util/CartCombiner.java
+++ src/main/java/com/nidas/app/etc/util/CartCombiner.java
@@ -22,11 +22,11 @@ public class CartCombiner {
 		
 		int addStock = 0;
 		// 시리얼, 사이즈를 기준으로 중복 검사한 뒤 수량을 합쳐줌
-		for (int i=0; i<originalCart.size()+inputCart.size(); i++) {
+		for (int i=0; i<originalCart.size(); i++) {
 			addStock = originalCart.get(i).getQuantity(); // i의 수량을 기본값으로 초기화
-			for (int j=i+1; j < originalCart.size()+inputCart.size(); j++) { // i의 다음 인덱스부터 중복값 검사
-				// 시리얼과 사이즈가 같다면 같은 제품으로 봄
-				if ( originalCart.get(i).getSerial() == originalCart.get(j).getSerial() && originalCart.get(i).getShoeSize() == originalCart.get(j).getShoeSize() ) {
+			for (int j=i+1; j < originalCart.size(); j++) { // i의 다음 인덱스부터 중복값 검사
+				// 시리얼과 사이즈가a 같다면 같은 제품으로 봄
+				if ( originalCart.get(i).getSerial().equals(originalCart.get(j).getSerial()) && originalCart.get(i).getShoeSize().equals(originalCart.get(j).getShoeSize()) ) {
 					addStock += originalCart.get(j).getQuantity(); // (i)의 기본값에 수량을 더해주고 삭제함
 					originalCart.get(i).setQuantity(addStock);
 					originalCart.remove(j);
--- src/test/java/com/nidas/app/test/Test.java
+++ src/test/java/com/nidas/app/test/Test.java
@@ -25,19 +25,75 @@ public class Test {
 		CartVO vo = new CartVO();
 		vo.setId("testuser");
 		vo.setSerial("315122-111");
-		vo.setShoeSize("s245");
+		vo.setShoeSize("s270");
 		vo.setQuantity(5);
 		
 		CartVO vo2 = new CartVO();
-		vo.setId("testuser");
-		vo.setSerial("315122-111");
-		vo.setShoeSize("s245");
-		vo.setQuantity(5);
+		vo2.setId("testuser");
+		vo2.setSerial("315122-666");
+		vo2.setShoeSize("s245");
+		vo2.setQuantity(5);
+		
+		CartVO vo3 = new CartVO();
+		vo3.setId("testuser");
+		vo3.setSerial("315122-111");
+		vo3.setShoeSize("s245");
+		vo3.setQuantity(5);
+		
+		CartVO vo4 = new CartVO();
+		vo4.setId("testuser");
+		vo4.setSerial("315122-111");
+		vo4.setShoeSize("s245");
+		vo4.setQuantity(5);
+		
+		CartVO vo5 = new CartVO();
+		vo5.setId("testuser");
+		vo5.setSerial("315122-666");
+		vo5.setShoeSize("s270");
+		vo5.setQuantity(5);
+		
+		
+		CartVO vo6 = new CartVO();
+		vo6.setId("testuser");
+		vo6.setSerial("315122-111");
+		vo6.setShoeSize("s245");
+		vo6.setQuantity(5);
+		
+		CartVO vo7 = new CartVO();
+		vo7.setId("testuser");
+		vo7.setSerial("315122-111");
+		vo7.setShoeSize("s245");
+		vo7.setQuantity(5);
+		
+		CartVO vo8 = new CartVO();
+		vo8.setId("testuser");
+		vo8.setSerial("315122-111");
+		vo8.setShoeSize("s245");
+		vo8.setQuantity(5);
+		CartVO vo9 = new CartVO();
+		vo9.setId("testuser");
+		vo9.setSerial("315122-111");
+		vo9.setShoeSize("s245");
+		vo9.setQuantity(5);
+		
+		
 		
 		origin.add(vo);
-		input.add(vo2);
+		origin.add(vo2);
+		origin.add(vo3);
+		origin.add(vo4);
+		origin.add(vo5);
+		origin.add(vo8);
+		input.add(vo6);
+		input.add(vo7);
+		input.add(vo8);
+		input.add(vo9);
 		List<CartVO> newList = CartCombiner.combine(origin, input);
 		System.out.println(newList.size());
+		System.out.println(newList.get(0).getShoeSize()+":"+newList.get(0).getSerial()+":"+newList.get(0).getQuantity());
+		System.out.println(newList.get(1).getShoeSize()+":"+newList.get(1).getSerial()+":"+newList.get(1).getQuantity());
+		System.out.println(newList.get(2).getShoeSize()+":"+newList.get(2).getSerial()+":"+newList.get(2).getQuantity());
+		System.out.println(newList.get(3).getShoeSize()+":"+newList.get(3).getSerial()+":"+newList.get(3).getQuantity());
 	}
 
 }
