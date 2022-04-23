85ee47c318f644ca5ad0aeae22964382c0168f10
Gu <Gu@nidas.shop>
2022-04-23 20:06:59 +0900

    ss

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/app-1.0.0.war
--- src/main/webapp/resources/js/header.js
+++ src/main/webapp/resources/js/header.js
@@ -7,3 +7,14 @@ function cateFadeOut(){
 	let reset = $('.hCateWrap').children();
 	reset.fadeOut();
 }
+
+$(function() {
+    let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    let thisPage = document.location.href;
+    
+    if (isMobile && thisPage.indexOf("http://m") == -1 ){
+    	location.href='http://m.nidas.shop';
+    } else if (!isMobile && thisPage.indexOf("http://www") == -1){
+    	location.href='http://www.nidas.shop';
+    }
+});
